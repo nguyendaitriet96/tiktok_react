d6621839ad7c242574c2303a66dbec1b2acfd7f8
nguyendaitriet96 <triet.nguyendai@smartdev.com>
2024-10-14 15:52:48 +0700

    Build base layout for header

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,5 @@
-function Header() {
-    return <h2>Header</h2>;
+function Sidebar() {
+    return <h2>Sidebar</h2>;
 }
 
-export default Header;
+export default Sidebar;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
-function Sidebar() {
-    return <h2>Sidebar</h2>;
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+function Header() {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('content')}></div>
+        </header>
+    );
 }
 
-export default Sidebar;
+export default Header;
